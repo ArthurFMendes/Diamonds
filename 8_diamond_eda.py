948b745ce16f314e7169ba52b0cad49a32e3f777
Arthur Mendes <44986466+ArthurFMendes@users.noreply.github.com>
2019-02-21 09:25:05 -0800

    Update 8_diamond_eda.py

--- 8_diamond_eda.py
+++ 8_diamond_eda.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
-Created on Sun Aug 12 02:36:22 2018
 
-@author: chase.kusterer
-
-Working Directory:
-/Users/chase.kusterer/Desktop/PyCourse
+@author: ArthurFMendes
 
 Purpose:
     This code is meant for exploratory data analysis (EDA) of the 
@@ -42,14 +37,6 @@ pd.np.corrcoef(diamonds['price'],
                diamonds['carat'])[1,0].round(2)
 
 
-# See Footnote 1 for an explanation of the code above
-
-
-"""
-Use the space below to check other correlations with pd.np.corrcoef
-"""
-
-
 
 ########################
 # Correlation matricies
@@ -62,12 +49,6 @@ print(df_corr)
 # Sending df_corr to Excel
 df_corr.to_excel("diamond_corr_matrix.xlsx")
 
-
-
-"""
-Let's focus on how price relates to other variables. We can do so with
-the code below.
-"""
 
 df_corr.loc['price'].sort_values(ascending = False)
 
@@ -88,14 +69,6 @@ plt.savefig('Diamond Correlation Heatmap.png')
 plt.show()
 
 
-# See Footnote 2 for an explanation of the code above
-
-
-"""
-Prof. Chase:
-    More info on cmap themes can be found here:
-    https://matplotlib.org/examples/color/colormaps_reference.html
-"""
 
 # Using palplot to view a color scheme
 sns.palplot(sns.color_palette('coolwarm', 12))
@@ -130,10 +103,6 @@ plt.xlabel('Carat Weight')
 plt.ylabel('Price')
 plt.grid(True)
 plt.show()
-
-
-# See Footnote 3 for an explanation of the code above
-
 
 
 # color and price
@@ -270,15 +239,6 @@ plt.savefig('Diamond Price Pairplot.png')
 plt.show()
 
 
-# See Footnote 4 for an explanation of the code above
-
-
-
-"""
-Prof. Chase:
-    sns.pairplot() can be slow on large datasets
-"""
-
 
 ###############################################################################
 # Do prices vary by store?
@@ -299,9 +259,6 @@ plt.tight_layout()
 plt.show()
 
 
-# See Footnote 5 for an explanation of the code above
-
-
 
 # it's hard to tell because the scatterplot is too crowded
 # let's break it down by channel
@@ -311,17 +268,6 @@ plt.show()
 # Relabeling channels and stores
 ########################
 
-"""
-Prof. Chase:
-    Let's relabel our channels and stores with actual names. It is a
-    bad practice to use numbers for categorical variables as it can
-    likely lead to mistakes (i.e. treating our variables as ordinal
-    data as opposed to categorical data).
-    
-    We can use .map to help us.
-""" 
-
-
 # Using dictionaries to make new labels for categorical variables
 diamonds['channel'] = diamonds['channel'].map(
         {0: 'mall',
@@ -330,9 +276,6 @@ diamonds['channel'] = diamonds['channel'].map(
 
     
 print(diamonds['channel'])
-
-
-# See Footnote 6 for an explanation of the code above
 
 
 
@@ -430,10 +373,6 @@ plt.show()
 # Violin plots
 ########################
 
-"""
-Prof. Chase:
-    Violin plots are a hybrid between boxplots and histograms.
-"""
 
 sns.violinplot(x = 'store',
                y = 'price',
@@ -466,7 +405,6 @@ plt.show()
 
 
 # Goodman's distribution looks very similar to the online retailers.
-# let's investigate.
 
 online_goodman = online_df.append(
         independent_df[independent_df['store'] == "Goodman's"])
@@ -480,15 +418,6 @@ sns.violinplot(x = 'store',
 
 plt.show()
 
-
-"""
-Prof. Chase:
-    The distributions via violin plots look similar. However,
-    distributions can be misleading as they are dependent on the
-    number of observations in the dataset.
-    
-    Let's investigate further using a hybrids of different plots.
-"""
 
 
 ########################
@@ -523,12 +452,6 @@ plt.show()
 
 ########################
 
-"""
-Prof. Chase:
-    This may be more visually dramatic if we utilized swarmplots to
-    fill in the values.
-"""
-
 sns.violinplot(x = 'store',
                y = 'price',
                data = online_goodman,
@@ -556,7 +479,6 @@ n_goodmans = len(diamonds[diamonds['store'] == "Goodman's"])
 
 
 print(f"""
-Prof. Chase:
     Goodman's appeared to exhibit a similar distribution to online
     retailers in the violin plot. However, after looking deeper, this
     retailer only has {n_goodmans} datapoints. This is much less than
@@ -572,153 +494,3 @@ Prof. Chase:
 ###############################################################################
 
 diamonds.to_excel('diamonds_explored.xlsx', index = False)
-
-
-
-"""
-###############################################################################
-# Footnotes
-###############################################################################
-
-
-Footnote 0: the purpose of footnotes
-
-to give a line-by-line explanation of a code snippet
-
-
-*******************************************************************************
-
-
-Footnote 1: calling a single correlation coefficient
- 
-pd.np.corrcoef(                      # calling the corrcoef function from pd.np
-diamonds['price'],                   # with price
-diamonds['carat'])                   # and carat
-[1,0]                                # ...and subsetting so that only one value is called instead of the entire matrix
-.round(2)                            # ...and finally rounding to two decimal places
-
-
-*******************************************************************************
-
-
-Footnote 2: utilizing sns.heatmap
-
-df_corr = diamonds.corr().round(2)   # creating a correlation matrix: df_corr
-
-sns.heatmap(                         # calling sns.heatmap
-df_corr,                             # ...on the matrix df_corr
-cmap = 'Blues',                      # setting the color theme to 'Blues'
-square = True,                       # making the plot print as a square
-annot = False,                       # not displaying numbers in the boxes
-linecolor = 'black',                 # setting line color (borders) to black
-linewidths = 0.5)                    # setting line width (borders) to 0.5
-
-
-plt.savefig('Diamond Correlation Heatmap.png')  # saving the figure
-plt.show()                                      # ...and displaying the plot
-
-
-*******************************************************************************
-
-
-Footnote 3: utilizing sns.heatmap
-
-df_corr = diamonds.corr().round(2)   # creating a correlation matrix: df_corr
-
-sns.heatmap(                         # calling sns.heatmap
-df_corr,                             # ...on the matrix df_corr
-cmap = 'Blues',                      # setting the color theme to 'Blues'
-square = True,                       # making the plot print as a square
-annot = False,                       # not displaying numbers in the boxes
-linecolor = 'black',                 # setting line color (borders) to black
-linewidths = 0.5)                    # setting line width (borders) to 0.5
-
-
-plt.savefig('Diamond Correlation Heatmap.png')  # saving the figure
-plt.show()                                      # ...and displaying the plot
-
-
-*******************************************************************************
-
-
-Footnote 4: utilizing sns.pairplot
-
-sns.pairplot(                                    # calling sns.pairplot
-data = diamonds,                                 # using the diamonds dataset
-x_vars = ['carat', 'color', 'clarity', 'cut'],   # with these variables on the x-axis
-y_vars = ['price'],                              # and this variable on the y-axis
-hue = 'channel',                                 # and also color-coding (hue) based on channel
-palette = 'plasma')                              # using the plasma color theme
-
-plt.tight_layout()                               # cleaning up the format of the plot
-plt.savefig('Diamond Price Pairplot.png')        # saving the figure
-plt.show()                                       # ...and displaying the plot
-
-
-*******************************************************************************
-
-
-Footnote 5: utilizing sns.lmplot
-
-sns.lmplot(                             # calling sns.lmplot
-x = 'carat',                            # with carat on the x-axis
-y = 'price',                            # and price on the y-axis
-data = diamonds,                        # using the diamonds dataset
-fit_reg = False,                        # specifying not to create a regresion line
-hue= 'store',                           # color-coding (hue) on store
-scatter_kws= {"marker": "D",  "s": 30}, # using the folowing customizations for themes               
-palette = 'plasma')                     # using the plasma color theme
-
-plt.title("Price and Carat by Store")   # specifying a custom title
-plt.grid()                              # specifying to plot gridlines
-plt.tight_layout()                      # cleaning up the layout
-plt.show()                              # ...and displaying the plot
-
-
-*******************************************************************************
-
-
-Footnote 6: mapping using a dictionary
-
-
-diamonds['channel'] =                   # setting channel from diamonds
-diamonds['channel'].map(                # using its old values and mapping them to new values
-{                                       # wrapper to open a dictionary
-0: 'mall',                              # channels with a value of 0 are now set to mall
-1: 'independent',                       # channels with a value of 1 are now set to independent
-2: 'online'                             # channels with a value of 2 are now set to online
-})                                      # wrapper to close a dictionary and the function
-
-    
-print(diamonds['channel'])              # printing the new values of channel
-
-
-*******************************************************************************
-
-
-Footnote 7: mapping using a dictionary
-
-
-sns.violinplot(                         # calling sns.violinplot
-x = 'store',                            # with store on the x-axis
-y = 'price',                            # and price on the y-axis
-data = online_goodman,                  # using the online_goodman dataset
-orient = 'v',                           # specifying vertical alignment
-inner = None,                           # specifying no fill
-color = 'white')                        # ...and a white background
-
-                                        # notice how there's no plt.show() here
-
-sns.stripplot(                          # calling sns.stripplot
-x = 'store',                            # with store on the x-axis
-y = 'price',                            # and price on the y-axis
-data = online_goodman,                  # using the online_goodman dataset
-jitter = True,                          # specifying jitter = True so that we can see overlapping data points more clearly
-size = 5,                               # specifying the size of each data point
-orient = 'v')                           # specifying vertical alignment
-
-
-plt.show()                              # ...and displaying the plot
-
-*******************************************************************************
-"""
